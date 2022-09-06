c17c5b700088a03d2a0f68d9c66a469da2c8ea53
ValkyrieQueen9 <valkyriannagames@gmail.com>
2022-09-06 13:54:18 +0100

    Working on UseIngredients Script

---
+++ We All Scream (For Ice Cream)/Assets/Scripts/IngredientList.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IngredientList
+{
+    Sprite galaxyIceCream = Resources.Load<Sprite>("galaxyIceCream");
+
+
+}
--- We All Scream (For Ice Cream)/Assets/Scripts/UseIngredient.cs
+++ We All Scream (For Ice Cream)/Assets/Scripts/UseIngredient.cs
@@ -10,13 +10,46 @@ public class UseIngredient : MonoBehaviour
     public bool sauce = false;
 
     private GameObject cone;
+    private GameObject scoop1;
+    private GameObject scoop2;
+    private SpriteRenderer Rend;
+    private Sprite galaxyFlavour, rainbowFlavour, galaxyScoop, rainbowScoop;
+    private SpriteRenderer coneRend;
+    private SpriteRenderer scoop1Rend;
+    private SpriteRenderer scoop2Rend;
+
 
     private void Start()
     {
-        cone = get
+        Rend = GetComponent<SpriteRenderer>();
+        galaxyFlavour = Resources.Load<Sprite>("flavours/galaxyIceCream");
+        rainbowFlavour = Resources.Load<Sprite>("flavours/rainbowIceCream");
+        galaxyScoop = Resources.Load<Sprite>("scoops/galaxyScoop");
+        rainbowScoop = Resources.Load<Sprite>("scoops/rainbowScoop");
+        cone = GameObject.FindGameObjectWithTag("Cone");
+        scoop1 = GameObject.Find("Scoop1");
+        scoop2 = GameObject.Find("Scoop2");
+        scoop1Rend = scoop1.GetComponent<SpriteRenderer>();
+        scoop2Rend = scoop2.GetComponent<SpriteRenderer>();
+
     }
 
-    //When ingredient (script is attached to each ingredient) is clicked check for which ingredient type it is. 
+    private void Update()
+    {
+           if (iceCream)
+        {
+            Rend.sprite = galaxyFlavour;
+            scoop1Rend.sprite = galaxyScoop;
+        }
+        if (topping)
+        {
+            Rend.sprite = rainbowFlavour;
+            scoop2Rend.sprite = rainbowScoop;
+        }
+        
+    }
+
+    //When ingredient (script is attached to each ingredient) is clicked check for which ingredient type and flavour it is. 
     //- use bools triggered when script is set to decide the items type e.g. if ingredient is sprinkles, mark topping as true in inspector 
 
     //When ingredient type found trigger special function e.g. ice cream function
@@ -24,6 +57,11 @@ public class UseIngredient : MonoBehaviour
     //In each ingredient function check whether any other ingredients have been placed, if none add in lowest tier.
     //Maybe use invisible sprites that switch out art when triggered rather than creating new objects??
 
-
+    void ChangeSprite()
+    {
+        SpriteRenderer currentSprite = GetComponent<SpriteRenderer>();
+        Sprite galaxyIceCream = Resources.Load<Sprite>("galaxyIceCream");
+        currentSprite.sprite = galaxyIceCream;
+    }
 
 }
