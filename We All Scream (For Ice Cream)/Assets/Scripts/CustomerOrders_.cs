e559c967377b8e5fd03b3e998912c0e27e0a2392
Anna <106345387+ValkyrieQueen9@users.noreply.github.com>
2022-11-14 00:40:39 +0000

    Customer Orders Progress
    Made While Loop and added all ticket sprites

--- We All Scream (For Ice Cream)/Assets/Scripts/CustomerOrders_.cs
+++ We All Scream (For Ice Cream)/Assets/Scripts/CustomerOrders_.cs
@@ -10,47 +10,54 @@ public class CustomerOrders_ : MonoBehaviour
     //----Add all ingredient Sprites - copy from use ingredients
     //New ingredient sprites needed for orders
     //----Create lists of each ingredient type and customers for each level
-    //Create randomiser function and output to inspector, then later, to order ticket
+    //---Create randomiser function and output to inspector
+    //Output order to order ticket
     //Create function to check submitted ice cream against randomised result
     //Separate boss customer order later
     //Create simple successful order counter.
 
-
-    public bool randomiserButton = false;
-    public string selection = "null";
+    public bool currentOrder = false;
 
     public string[] ingredients = new string[15];
 
     //Orders
-    public string ticketIceCream1;
-    public string ticketIceCream2;
-    public string ticketIceCream3;
-    public string ticketIceCream4;
-    public string ticketIceCream5;
-    public string ticketSauce;
-    public string ticketTopping;
+    public string orderCone;
+    public string orderIceCream1;
+    public string orderIceCream2;
+    public string orderIceCream3;
+    public string orderIceCream4;
+    public string orderIceCream5;
+    public string orderSauce;
+    public string orderTopping;
 
     //Renderers
     private SpriteRenderer Rend;
 
-    //Flavours
+    //Ticket Ingredients Sprites
+    private Sprite ticketPlainCone, ticketBoneScoop, ticketCosmicScoop, ticketMagmaScoop, ticketMeatScoop, ticketTropicalScoop, ticketAcidSauce, ticketBloodySauce, ticketBloodyUnicornSauce, ticketSoulSauce, ticketBatteriesTop, ticketEyeballsTop, ticketGemsTop, ticketGlassTop, ticketNettlesTop;
+
+    //Flavours Sprites
     private Sprite plainCone, boneScoop, cosmicScoop, magmaScoop, meatScoop, tropicalScoop, acidSauce, bloodySauce, bloodyUnicornSauce, soulSauce, batteriesTop, eyeballsTop, gemsTop, glassTop, nettlesTop;
 
-    //Cone
+    //Cone Builder
     private GameObject buildCone;
     private SpriteRenderer buildConeRend;
 
-    //Scoops
+    //Scoops Builder
     private GameObject scoop1, scoop2, scoop3, scoop4, scoop5;
     private SpriteRenderer scoop1Rend, scoop2Rend, scoop3Rend, scoop4Rend, scoop5Rend;
 
-    //Sauce
+    //Sauce Builder
     private GameObject sauce1, sauce2, sauce3, sauce4, sauce5;
     private SpriteRenderer sauce1Rend, sauce2Rend, sauce3Rend, sauce4Rend, sauce5Rend;
 
-    //Toppings
+    //Toppings Builder
     private GameObject topping1, topping2, topping3, topping4, topping5;
     private SpriteRenderer topping1Rend, topping2Rend, topping3Rend, topping4Rend, topping5Rend;
+
+    //Order Ticket Builder
+    private GameObject ticketCone, ticketScoop1, ticketScoop2, ticketScoop3, ticketScoop4, ticketScoop5, ticketSauce, ticketTopping;
+    private SpriteRenderer ticketConeRend, ticketScoop1Rend, ticketScoop2Rend, ticketScoop3Rend, ticketScoop4Rend, ticketScoop5Rend, ticketSauceRend, ticketToppingRend; 
 
 
     private void Start()
@@ -76,6 +83,27 @@ public class CustomerOrders_ : MonoBehaviour
         gemsTop = Resources.Load<Sprite>("toppings/gemsTopping");
         glassTop = Resources.Load<Sprite>("toppings/glassTopping");
         nettlesTop = Resources.Load<Sprite>("toppings/nettlesTopping");
+
+        //Ticket Ingredient Sprites
+        
+        ticketPlainCone = Resources.Load<Sprite>("cones/plainCone");
+
+        ticketBoneScoop = Resources.Load<Sprite>("scoops/boneScoop");
+        ticketCosmicScoop = Resources.Load<Sprite>("scoops/cosmicScoop");
+        ticketMagmaScoop = Resources.Load<Sprite>("scoops/magmaScoop");
+        ticketMeatScoop = Resources.Load<Sprite>("scoops/meatScoop");
+        ticketTropicalScoop = Resources.Load<Sprite>("scoops/tropicalScoop");
+
+        ticketAcidSauce = Resources.Load<Sprite>("sauces/acidSauce");
+        ticketBloodySauce = Resources.Load<Sprite>("sauces/bloodySauce");
+        ticketBloodyUnicornSauce = Resources.Load<Sprite>("sauces/bloodyUnicornSauce");
+        ticketSoulSauce = Resources.Load<Sprite>("sauces/soulSauce");
+
+        ticketBatteriesTop = Resources.Load<Sprite>("toppings/batteriesTopping");
+        ticketEyeballsTop = Resources.Load<Sprite>("toppings/eyeballsTopping");
+        ticketGemsTop = Resources.Load<Sprite>("toppings/gemsTopping");
+        ticketGlassTop = Resources.Load<Sprite>("toppings/glassTopping");
+        ticketNettlesTop = Resources.Load<Sprite>("toppings/nettlesTopping");
 
         //Cone Renderer
         buildCone = GameObject.FindGameObjectWithTag("Cone");
@@ -117,6 +145,24 @@ public class CustomerOrders_ : MonoBehaviour
         topping4Rend = topping4.GetComponent<SpriteRenderer>();
         topping5Rend = topping5.GetComponent<SpriteRenderer>();
 
+        //Order Ticket Renderer
+        ticketCone = GameObject.Find("TicketCone");
+        ticketScoop1 = GameObject.Find("TicketScoop1");
+        ticketScoop2 = GameObject.Find("TicketScoop2");
+        ticketScoop3 = GameObject.Find("TicketScoop3");
+        ticketScoop4 = GameObject.Find("TicketScoop4");
+        ticketScoop5 = GameObject.Find("TicketScoop5");
+        ticketSauce = GameObject.Find("TicketSauce");
+        ticketTopping = GameObject.Find("TicketTopping");
+        ticketConeRend = ticketCone.GetComponent<SpriteRenderer>();
+        ticketScoop1Rend = ticketScoop1.GetComponent<SpriteRenderer>();
+        ticketScoop2Rend = ticketScoop2.GetComponent<SpriteRenderer>();
+        ticketScoop3Rend = ticketScoop3.GetComponent<SpriteRenderer>();
+        ticketScoop4Rend = ticketScoop4.GetComponent<SpriteRenderer>();
+        ticketScoop5Rend = ticketScoop5.GetComponent<SpriteRenderer>();
+        ticketSauceRend = ticketSauce.GetComponent<SpriteRenderer>();
+        ticketToppingRend = ticketTopping.GetComponent<SpriteRenderer>();
+
         //Cone
         ingredients[0] = "plainCone";
         //Scoops
@@ -140,20 +186,35 @@ public class CustomerOrders_ : MonoBehaviour
 
     private void Update()
     {
-        if (randomiserButton)
+
+        while (currentOrder == false)
         {
-            //Cone randomiser not needed currently
-            ticketIceCream1 = ingredients[Randomiser(1, 5)];
-            ticketIceCream2 = ingredients[Randomiser(1, 5)];
-            ticketIceCream3 = ingredients[Randomiser(1, 5)];
-            ticketIceCream4 = ingredients[Randomiser(1, 5)];
-            ticketIceCream5 = ingredients[Randomiser(1, 5)];
-            ticketSauce = ingredients[Randomiser(6, 9)];
-            ticketTopping = ingredients[Randomiser(10, 14)];
+
+            //Cone?
+            orderCone = ingredients[0];
+
+            //Scoops
+            orderIceCream1 = ingredients[Randomiser(1, 6)];
+            orderIceCream2 = ingredients[Randomiser(1, 6)];
+            orderIceCream3 = ingredients[Randomiser(1, 6)];
+            orderIceCream4 = ingredients[Randomiser(1, 6)];
+            orderIceCream5 = ingredients[Randomiser(1, 6)];
+
+            //Sauce
+            orderSauce = ingredients[Randomiser(6, 10)];
+
+            //Topping
+            orderTopping = ingredients[Randomiser(10, 15)];
+
+            currentOrder = true;
+            //Trigger false when an order is successfully submitted.
         }
-        //selection = ticketIceCream1;
-        //Debug.Log(selection.ToString());
-        //Debug.Log("Randomiser: " + Randomiser(1, 9).ToString());
+
+        if (orderIceCream1 == ingredients[1])
+        {
+            ticketScoop1Rend.sprite = ticketBoneScoop;
+        }
+      
     }
 
     private int Randomiser(int x, int y)
